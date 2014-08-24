f8a207fdd7543a301d018d6f04f159ef448c4ee3
Jerry Su <email@jerrysu.me>
2014-08-23 19:36:44 -0700

    Switch from Grunt to Gulp. This took a hell of a lot of time to figure out..

---
+++ gulpfile.js
@@ -0,0 +1,127 @@
+'use strict';
+
+var gulp = require('gulp');
+var autoprefixer = require('gulp-autoprefixer');
+var cache = require('gulp-cached');
+var csslint = require('gulp-csslint');
+var cssmin = require('gulp-minify-css');
+var htmlmin = require('gulp-htmlmin');
+var imagemin = require('gulp-imagemin');
+var revall = require('gulp-rev-all');
+
+var browserSync = require('browser-sync');
+var rimraf = require('rimraf');
+var rsync = require('rsyncwrapper').rsync;
+var runSequence = require('run-sequence');
+
+var pkg = require('./package.json');
+
+gulp.task('clean', function(next) {
+  rimraf('build', next);
+});
+
+gulp.task('copy', ['clean'], function() {
+  return gulp.src('src/**').pipe(gulp.dest('build'));
+});
+
+gulp.task('build', ['copy'], function(next) {
+  runSequence(['build-css', 'build-html'], next);
+});
+
+gulp.task('build-css', function() {
+  return gulp.src(['src/**/*.css', '!src/**/*.min.css'])
+    .pipe(cache('css'))
+    .pipe(autoprefixer({
+      cascade: false /* Note: This doesn't seem to work. */
+    }))
+    .pipe(gulp.dest('build'))
+    .pipe(browserSync.reload({stream: true}));
+});
+
+gulp.task('build-html', function() {
+  return gulp.src('src/**/*.html')
+    .pipe(cache('html'))
+    .pipe(gulp.dest('build'))
+    .pipe(browserSync.reload({stream: true}));
+});
+
+gulp.task('cdn', ['build'], function() {
+  return gulp.src('build/**')
+    .pipe(revall({
+      ignore: [/^\/favicon.ico$/, '.html', /^\/fonts\//,]
+    }))
+    .pipe(gulp.dest('build'));
+});
+
+gulp.task('minify', ['clean', 'copy', 'build', 'cdn'], function(next) {
+  runSequence(['minify-css', 'minify-html'], next);
+});
+
+gulp.task('minify-css', function() {
+  return gulp.src('build/**/*.css')
+    .pipe(cssmin({
+      root: '.',
+      relativeTo: 'build',
+      processImport: true
+    }))
+    .pipe(gulp.dest('build'));
+});
+
+gulp.task('minify-html', function() {
+  return gulp.src('build/**/*.html')
+    .pipe(htmlmin({
+      removeComments: true,
+      removeCommentsFromCDATA: true,
+      removeCDATASectionsFromCDATA: true,
+      collapseWhitespace: true,
+      collapseBooleanAttributes: true,
+      removeAttributeQuotes: true,
+      removeRedundantAttributes: true,
+      useShortDoctype: true,
+      removeEmptyAttributes: true,
+      removeScriptTypeAttributes: true,
+      removeStyleLinkTypeAttributes: true,
+      minifyJS: true,
+      minifyCSS: true
+    }))
+    .pipe(gulp.dest('build'));
+});
+
+gulp.task('optimize-images', function() {
+  return gulp.src('src/images/**')
+    .pipe(imagemin({
+      progressive: true
+    }))
+    .pipe(gulp.dest('src/images/'));
+});
+
+gulp.task('csslint', function() {
+  gulp.src('src/**/*.css')
+    .pipe(csslint())
+    .pipe(csslint.reporter());
+});
+
+gulp.task('server', ['clean', 'copy', 'build'], function(next) {
+  gulp.watch('src/**/*.css', ['build-css']);
+  gulp.watch('src/**/*.html', ['build-html']);
+
+  browserSync({server: {baseDir: 'build'}}, next);
+});
+
+gulp.task('deploy', ['clean', 'copy', 'build', 'cdn', 'minify'], function(next) {
+  // Note: Maybe create a Gulp plugin for this?
+  rsync({
+    ssh: true,
+    src: 'build/',
+    dest: pkg.targets.prod.host + ':' + pkg.targets.prod.path,
+    recursive: true,
+    args: ['--verbose']
+  }, function(err, stdout, stderr, cmd) {
+    console.log(stdout);
+    next();
+  });
+});
+
+gulp.task('default', function() {
+  gulp.start('server');
+});
